30a8645b2c2ad1b5877ea7081a3e7a777f944a2d
sao <wllmgr@hotmail.com>
2023-11-27 12:05:30 -0500

    async-landing V 1.0.0.1 modify main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -34,7 +34,7 @@ async function fetchData(urlApi) {
 					</h3>
 				</div>
 			</div>
-		`).slice(0, 10).join('')}`;
+		`).slice(0, 4).join('')}`;
 		content.innerHTML = view;
 	} catch (error) {
 		console.log(error);
